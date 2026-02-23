4caeee0d46aa26c7132d2c90b6f9478c0a2a342b
KrishnaGrg1 <gkrishnabahadur618@gmail.com>
2026-02-24 00:47:48 +0545

    feat: add AuthModule to OrderModule imports

--- apps/backend/src/order/order.module.ts
+++ apps/backend/src/order/order.module.ts
@@ -5,9 +5,10 @@ import { EmailModule } from 'src/email/email.module';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { OrderController } from './order.controller';
 import { OrderServices } from './order.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [ConfigModule, EmailModule, CloudinaryModule],
+  imports: [ConfigModule, EmailModule, CloudinaryModule, AuthModule],
   controllers: [OrderController],
   providers: [PrismaService, OrderServices],
 })
