fe1c69c3e1e61f07e00984111bc4f4ae43834984
KrishnaGrg1 <gkrishnabahadur618@gmail.com>
2026-02-23 18:56:14 +0545

    Refactor code structure for improved readability and maintainability

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { devtools } from '@tanstack/devtools-vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import { fileURLToPath, URL } from 'url'
+import { nitro } from 'nitro/vite'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 const config = defineConfig({
@@ -24,6 +25,7 @@ const config = defineConfig({
   plugins: [
     devtools(),
     tanstackStart(),
+        nitro(),
     viteReact(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
